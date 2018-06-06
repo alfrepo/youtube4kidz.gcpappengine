2c1921eb558e497194056135893859962bbf72d0
anena <anenada@gmail.com>
2018-06-06 13:07:03 +0200

    Removed the test code from the application start

--- src/main/java/digital/alf/youtube4kidz/DemoApplication.java
+++ src/main/java/digital/alf/youtube4kidz/DemoApplication.java
@@ -27,12 +27,6 @@ public class DemoApplication {
 
     @GetMapping("/")
     public String hello() {
-        Video v = new Video();
-        v.setAuthor("Me");
-        v.setName("Me");
-        v.setYoutubeLink("Link");
-        videoDao.createVideo(v);
-
         List<User> users = userDao.listUsers();
         List<Video> videos = videoDao.getVideos();
         return String.format("hello world! VideoResource: %s, Users: %s", videos.size(), users.size());
